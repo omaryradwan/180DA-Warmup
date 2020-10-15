649a72e53d6d5a99d0c982041f3ca1c41840fe3c
Omar Radwan <omaryradwan@gmail.com>
2020-10-15 04:18:35 +0100

    New Pi Stuff 2.0 for lab report

---
+++ pi.py
@@ -0,0 +1,8 @@
+if __name__ == '__main__':
+    x = "Raspberry Pi "
+    if x == "Raspberry Pi":
+        print("You are living in 2017");
+    else:
+        # this is acomment
+        x = x + " - We are hosted on this system"
+        print(x)
